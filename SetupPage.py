9c723387f5eb7147489f6d693d272ebedaf2e7bb
BrainNotRespondin <66655892+BrainNotRespondin@users.noreply.github.com>
2020-06-22 09:23:18 +1000

    Update SetupPage.py

--- SetupPage.py
+++ SetupPage.py
@@ -1,3 +1,7 @@
+name = float(input("Hello and welcome to Lifeline, please enter your name"))
+age = float(input("Please enter your age"))
+
+
 height = float(input("Input your height in meters: "))
 weight = float(input("Input your weight in kilogram: "))
 print("Your body mass index is: ", round(weight / (height * height), 2))
