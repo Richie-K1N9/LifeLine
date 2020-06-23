9751b8d0c5ea2e81ea7ec61e744cd6b0d4bbd74c
BrainNotRespondin <66655892+BrainNotRespondin@users.noreply.github.com>
2020-06-23 17:06:12 +1000

    Update SetupPage.py

--- SetupPage.py
+++ SetupPage.py
@@ -1,9 +1,31 @@
-name = (input("Hello and welcome to Lifeline, please enter your name"))
+name = (input("Hello and welcome to Lifeline, please enter your name: "))
 print("Hello," + name)
-age = (input("Please enter your age"))
+age = (input("Please enter your age: "))
 
-height = (input("Input your height in meters: "))
-weight = (input("Input your weight in kilogram: "))
+height = float(input("Enter height in meters: "))
+
+weight = float(input("Enter weight in kg: "))
+
 bmi = weight/(height**2)
-print(bmi)
+
+bmi2 = round(bmi,2)
+
+
+print("Your BMI is: {0} and you are: ".format(bmi2), end='')
+
+if ( bmi < 16):
+   print("severely underweight")
+
+elif ( bmi >= 16 and bmi < 18.5):
+   print("underweight")
+
+elif ( bmi >= 18.5 and bmi < 25):
+   print("Healthy")
+
+elif ( bmi >= 25 and bmi < 30):
+   print("overweight")
+
+elif ( bmi >=30 and bmi <35):
+   print("severely overweight")
+
 
