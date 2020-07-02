94d166d7da759ef98ac761a3b43fcb1612b612ba
Richie King <Richie.K1N9@gmail.com>
2020-07-02 13:17:00 +1000

    template

---
+++ TEMPLATE.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import sqlite3
+
+#Startup
+root = Tk()
+#root.overrideredirect(True)
+root.iconbitmap(r'C:\Users\Richie.King\Documents\GitHub\LifeLine\images\dark_logo.ico') #Icon
+root.title("LifeLine") #Title
+root.geometry("750x450") #Menu Size
+#root.configure(bg='#23272A') #Background Colour
+
+root.mainloop()
