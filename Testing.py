f30aa63373cc23f3b30e016f4b134e7eadbb5928
BrainNotRespondin <66655892+BrainNotRespondin@users.noreply.github.com>
2020-07-02 13:44:38 +1000

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -2,13 +2,6 @@ import tkinter as tk
 
 window = tk.Tk()
 label = tk.Label(text="Welcome to Lifeline")
-button = tk.Button(
-    text="Click me!",
-    width=25,
-    height=5,
-    bg="blue",
-    fg="yellow",
-)
 label.pack()
 
 window.mainloop()
