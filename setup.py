9ef08fbf531bd6ebbbefad101c3f4d7d62553312
Richie King <Richie.K1N9@gmail.com>
2020-07-03 12:28:06 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,18 +6,32 @@ root = Tk()
 #root.overrideredirect(True)
 #root.iconbitmap(r'C:\Users\Richie.King\Documents\GitHub\LifeLine\images\dark_logo.ico') #Icon
 root.title("LifeLine") #Title
-root.geometry("750x450") #Menu Size
+root.geometry("350x450") #Menu Size
 #root.configure(bg='#23272A') #Background Colour
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="yourusername",
-    password="yourpassword"
-)
+title = Label(root, text = "Welcome to LifeLine")
+f_name = Label(root, text = "First Name")
+l_name = Label(root, text = "Last Name")
+dob = Label(root, text = "Date of Birth")
+height = Label(root, text = "Height")
+weight = Label(root, text = "Weight")
 
-mycursor = mydb.cursor()
+f_name_box = Entry(root)
+l_name_box = Entry(root)
+dob_box = Entry(root)
+height_box = Entry(root)
+weight_box = Entry(root)
 
-mycursor.execute("CREATE DATABASE user")
-
+title.grid(row= 2, column= 3)
+f_name.grid(row= 3, column= 2)
+f_name_box.grid(row= 3, column= 4)
+l_name.grid(row= 4, column= 2)
+l_name_box.grid(row= 4, column= 4)
+dob.grid(row= 5, column= 2)
+dob_box.grid(row= 5, column= 4)
+height.grid(row= 6, column= 2)
+height_box.grid(row= 6, column= 4)
+weight.grid(row= 7, column= 2)
+weight_box.grid(row= 7, column= 4)
 
 root.mainloop()
