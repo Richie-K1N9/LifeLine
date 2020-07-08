02d488fbd474141942e57790a0602a0a73e64dc4
Richie-K1N9 <56552730+Richie-K1N9@users.noreply.github.com>
2020-07-08 13:10:53 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,37 +1,70 @@
 from tkinter import *
-import sqlite3
+import tkinter as tk
+import tkinter.font as font
+import mysql.connector
 
-#Startup
+#Window properties
 root = Tk()
 #root.overrideredirect(True)
-#root.iconbitmap(r'C:\Users\Richie.King\Documents\GitHub\LifeLine\images\dark_logo.ico') #Icon
+root.iconbitmap(r'C:\Users\Richie.King\Documents\GitHub\LifeLine\images\dark_logo.ico') #Icon
 root.title("LifeLine") #Title
-root.geometry("350x450") #Menu Size
-#root.configure(bg='#23272A') #Background Colour
+root.geometry("475x450") #Menu Size
+root.configure(bg='#23272A') #Background Colour
 
+#Database
+#db = mysql.connector.connect(
+#    host="localhost",
+#    user="yourusername",
+#    password="yourpassword",
+#    database="data"
+#)
+#
+#mycursor = db.cursor()
+#
+#mycursor.execute("CREATE TABLE data (f_name VARCHAR(255), l_name VARCHAR(255))")
+
+#Bold font var
+h_f = font.Font(weight="bold", size=20)
+
+#Info
 title = Label(root, text = "Welcome to LifeLine")
+title['font'] = h_f
 f_name = Label(root, text = "First Name")
 l_name = Label(root, text = "Last Name")
-dob = Label(root, text = "Date of Birth")
+dob = Label(root, text = "Age")
 height = Label(root, text = "Height")
 weight = Label(root, text = "Weight")
 
-f_name_box = Entry(root)
-l_name_box = Entry(root)
-dob_box = Entry(root)
-height_box = Entry(root)
-weight_box = Entry(root)
+f_data = tk.StringVar()
+l_data = tk.StringVar()
+dob_data = tk.StringVar()
+height_data = tk.StringVar()
+weight_data = tk.StringVar()
 
-title.grid(row= 2, column= 3)
-f_name.grid(row= 3, column= 2)
-f_name_box.grid(row= 3, column= 4)
-l_name.grid(row= 4, column= 2)
-l_name_box.grid(row= 4, column= 4)
-dob.grid(row= 5, column= 2)
-dob_box.grid(row= 5, column= 4)
-height.grid(row= 6, column= 2)
-height_box.grid(row= 6, column= 4)
-weight.grid(row= 7, column= 2)
-weight_box.grid(row= 7, column= 4)
+#Text Boxes
+f_name_box = Entry(root, textvariable=f_data)
+l_name_box = Entry(root, textvariable=l_data)
+dob_box = Entry(root, textvariable=dob_data)
+height_box = Entry(root, textvariable=height_data)
+weight_box = Entry(root, textvariable=weight_data)
+
+#Canvas
+title.grid(row= 2, column= 3, pady=15)
+f_name.grid(row= 3, column= 2, pady=5)
+l_name.grid(row= 4, column= 2, pady=5)
+dob.grid(row= 5, column= 2, pady=5)
+height.grid(row= 6, column= 2, pady=5)
+weight.grid(row= 7, column= 2, pady=5)
+f_name_box.grid(row= 3, column= 4, pady=5)
+l_name_box.grid(row= 4, column= 4, pady=5)
+dob_box.grid(row= 5, column= 4, pady=5)
+height_box.grid(row= 6, column= 4, pady=5)
+weight_box.grid(row= 7, column= 4, pady=5)
+
+#Output Data
+well = Label(root, text="Welcome " + str(f_name))
+
+#Output Grid
+well.grid(row= 8, column=2, pady=10)
 
 root.mainloop()
